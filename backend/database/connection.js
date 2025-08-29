e30d389dcdea77d699a27efc54cc066247738b7d
framirezj <12649259+framirezj@users.noreply.github.com>
2025-08-28 20:38:42 -0400

    feat(db): create conection to database

---
+++ backend/database/connection.js
@@ -0,0 +1,21 @@
+import pkg from "pg";
+const { Pool } = pkg;
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+export const pool = new Pool({
+  user: process.env.USER || "postgres",
+  host: process.env.HOST || "localhost",
+  database: process.env.DATABASE || "db_app_todo",
+  password: process.env.PASSWORD || "postgres",
+  port: process.env.PORT || 5432,
+  allowExitOnIdle: true,
+});
+
+try {
+    await pool.query("SELECT NOW()")
+    console.log("Database Connected 😎")
+} catch (error) {
+    console.log(error)
+}
---
+++ backend/models/posts.model.js
@@ -0,0 +1,41 @@
+import { pool } from '../database/connection.js'
+
+const table = "posts"
+
+
+
+/* export const findAll = async () => {
+    const res = await pool.query("SELECT * FROM todos")
+    return res.rows
+}
+
+export const findById = async (id) => {
+
+    const query = "SELECT * FROM todos WHERE id = $1"
+    const values = [id]
+
+    const res = await pool.query(query, values)
+    return res.rows[0]
+
+}
+
+export const create = async (title) => {
+    const query = "INSERT INTO todos (title) VALUES ($1) RETURNING *"
+    const values = [title]
+
+    const res = await pool.query(query, values)
+
+    return res.rows[0]
+}
+
+export const remove = async (id) => {
+    const query = "DELETE FROM todos WHERE id = $1 RETURNING *"
+    const res = await pool.query(query,[id])
+    return res.rows[0]
+}
+
+export const update = async (id) => {
+    const query = "UPDATE todos SET done = NOT done WHERE id = $1 RETURNING *"
+    const res = await pool.query(query,[id])
+    return res.rows[0]
+} */
